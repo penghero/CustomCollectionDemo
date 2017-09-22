4bf8fd9260b0903929994999dc3193150f7988e7
陈鹏 <chen@chenpengs-MacBook-Air.local>
2017-09-22 16:45:20 +0800

    Initial Commit

---
+++ CustomCollectionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomCollectionDemo
+//
+//  Created by 陈鹏 on 2017/9/22.
+//  Copyright © 2017年 penggege.CP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CustomCollectionDemoTests/CustomCollectionDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  CustomCollectionDemoTests.m
+//  CustomCollectionDemoTests
+//
+//  Created by 陈鹏 on 2017/9/22.
+//  Copyright © 2017年 penggege.CP. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomCollectionDemoTests : XCTestCase
+
+@end
+
+@implementation CustomCollectionDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
